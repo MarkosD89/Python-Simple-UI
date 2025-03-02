91ff9667b4312dfbf820a1d43e70baed70876fbd
MarkosD89 <mdagios@gmail.com>
2025-03-02 13:43:15 +0000

    Delete Dependencies directory

--- Dependencies/Processes.py
+++
@@ -1,5 +0,0 @@
-#############################################################################################
-# This code is property of the individual created:
-# All rights to the code are reserved
-# Unothorized use, disctrubution or any adaptation of the code will result in ligal actions
-#############################################################################################
